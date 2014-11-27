bbbd60e86edf3c184f12f8059fe5866ef601f298
Nilssonnet <nilssonnet@gmail.com>
2014-11-27 23:29:26 +0100

    Not working

--- Assignment1/Achtung!/app/src/main/java/com/mattias/achtung/MainActivity.java
+++ Assignment1/Achtung!/app/src/main/java/com/mattias/achtung/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mattias.achtungalleschat;
+package com.mattias.achtung;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class LoginActivity extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
