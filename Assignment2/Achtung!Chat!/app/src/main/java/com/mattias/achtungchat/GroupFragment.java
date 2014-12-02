32640b20251e4330431056f41162097cd2115374
Nilssonnet <nilssonnet@gmail.com>
2014-12-03 00:21:15 +0100

    Updated assignment2 with code from assignment1

--- Assignment2/Achtung!Chat!/app/src/main/java/com/mattias/achtungchat/GroupFragment.java
+++ Assignment2/Achtung!Chat!/app/src/main/java/com/mattias/achtungchat/GroupFragment.java
@@ -1,30 +1,33 @@
 package com.mattias.achtungchat;
 
 
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GroupFragment extends Fragment {
-
-
-    public GroupFragment() {
-        // Required empty public constructor
-    }
-
+public class GroupFragment extends ListFragment {
+    private String[] listString = {"Chat here", "Chat there", "Chat everywhere"};
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().setTitle("Groups");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, listString);
+        setListAdapter(adapter);
     }
 
-
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String clicked = (String) l.getItemAtPosition(position);
+        ((MainActivity) getActivity()).changeFragments(clicked);
+    }
 }
