afbe2a61d38a92e1915fad39a6d35f6cf7387f5c
Nilssonnet <nilssonnet@gmail.com>
2015-01-07 20:13:35 +0100

    Started working on assignment4

---
+++ Assignment4/BoomBox/app/src/main/java/com/mattias/boombox/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mattias.boombox;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivity extends ActionBarActivity {
+    private FragmentManager fragmentManager;
+    private PlayFragment playFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null){
+            fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            playFragment = new PlayFragment();
+            transaction.replace(R.id.container_main, playFragment);
+            transaction.commit();
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
